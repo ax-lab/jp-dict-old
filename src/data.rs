5446d7c260cbd73982ce13c15fc81ebd98e95c9c
Ricardo B. Machado <ricardobm@gmail.com>
2020-03-21 12:20:09 -0300

    Conditional embedding of dictionary data on build
    - Disables embedding on debug builds to decrease compilation times
    - Add `no-embed` feature to disable embedding even on release

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,23 @@
 use db::DB;
 
+#[cfg(any(debug_assertions, feature = "no-embed"))]
+pub fn get_db() -> &'static DB<'static> {
+	lazy_static! {
+		static ref DATA: Vec<u8> = {
+			let mut dict_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+			dict_path.push("data/dictionary.in");
+			std::fs::read(dict_path).unwrap()
+		};
+		static ref DATABASE: DB<'static> = DB::load(&DATA[..]);
+	}
+	&DATABASE
+}
+
+#[cfg(not(any(debug_assertions, feature = "no-embed")))]
 static DATA: &[u8] = include_bytes!("../data/dictionary.in");
 
+#[cfg(not(any(debug_assertions, feature = "no-embed")))]
+#[inline]
 pub fn get_db() -> &'static DB<'static> {
 	lazy_static! {
 		static ref DATABASE: DB<'static> = DB::load(DATA);
